e96565f57ecdfc8f8254312bff4d0d6b501bbe5a
Jotar Alvaro <jotaralvaro@gmail.com>
2021-07-24 17:18:26 -0300

    tomar datos de nuevas notas JS

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-var ID = function () {
-    return '_' + Math.random().toString(36).substr(2, 9);
-  };
+const button = document.getElementById("btn");
+button.addEventListener('click', tomarDatos);
+function tomarDatos() {
+    const titulo = document.getElementById("boton_titulo");
+    const contenido = document.getElementById("exampleFormControlTextarea1");
+    const categoria = document.getElementById("categoria");
+    const nuevaNota = {
+        id: "id",
+        titulo: titulo.value,
+        contenido: contenido.value,
+        categoria: categoria.value
+    }
+    
+    let ID = function () {
+        return '_' + Math.random().toString(36).substr(2, 9);
+    }
+    nuevaNota["id"] = ID
+    
+    console.log(nuevaNota);
 
-ID ();
+
+}
+
